8566f9cdbc01e8a56508aa4eed456d802b10ae83
sebastian.baltschun <sebastian.baltschun@rwth-aachen.de>
2026-07-15 15:00:07 +0200

    Moved material constants from transfer_matrix_3d to GrandPropagationMatrix

--- test.jl
+++ test.jl
@@ -143,37 +143,6 @@ function propagationCoeffs(freq::Real,
     return P
 end
 
-# function axionModes(coords::Coordinates,modes::Modes,freqs::AbstractVector{<:Real},
-#         tilts::AbstractVector{<:Real}; velocity_x::Real=0)
-
-#     d = fieldDims(modes)
-#     Ea = Matrix{ComplexF64}(undef,length(coords.X),length(coords.X))
-
-
-#     ML = modes.M*(2modes.L+1)
-#     ax = Array{ComplexF64}(undef,length(freqs),length(tilts),length(tilts),ML)
-
-#     for ty in eachindex(tilts), tx in eachindex(tilts)
-#         for f in eachindex(freqs)
-#             ka = 2π*freqs[f]/c0
-
-#             Ea .= 1; Ea .*= coords.diskmaskin
-            
-#             # inaccuracies of the emitted fields: B-field and velocity effects
-#             if velocity_x != 0
-#                 for (i,x) in enumerate(coords.X); Ea[i,:] .*= cis(ka*x*velocity_x); end
-#             end
-
-#             tiltField!(Ea,ka,coords,tilts[tx],tilts[ty])
-
-#             coeffs = modeDecomp(Ea,modes)
-#             @views copyto!(ax[f,tx,ty,:],coeffs)
-#         end
-#     end
-
-#     return ax
-# end
-
 mutable struct GrandPropagationMatrix
     """
     Precalculates the propagation matrices for a set of distances and relative tilts to later interpolate from.
@@ -181,33 +150,36 @@ mutable struct GrandPropagationMatrix
     P_t contains the spline for tilt interpolation.
     """
     freqs::AbstractArray{<:Real}
-    # thickness::Float64
-    # nd::ComplexF64
+    thickness::Float64
+    eps::Float64
+    tand::Float64
+    nm::Float64
 
     M::Int
     L::Int
     ML::Int
 
-    #ax::ScaledInterpolation
     P_d::ScaledInterpolation
     P_t::ScaledInterpolation
+    P_disk::ScaledInterpolation
 
     function GrandPropagationMatrix(freqs::AbstractArray{<:Real},
             modes::Modes,
-            coords::Coordinates;)
+            coords::Coordinates; 
+            eps=24.0,
+            tand=0.,
+            nm=1e30,
+            thickness=1e-3)
         
-        distances = range(1e-3, 9e-3, 50)
+        distances = range(0e-3, 10e-3, 50)
         tilts = range(deg2rad(-0.05), deg2rad(0.05), 10)
 
         M = modes.M; L = modes.L; ML = M*(2L+1)
         p_d = Array{ComplexF64}(undef,length(freqs),length(distances),ML,ML)
         p_t = Array{ComplexF64}(undef,length(freqs),length(tilts),length(tilts),ML,ML)
+        p_disk = Array{ComplexF64}(undef,length(freqs),length(distances),ML,ML)
         bc = BSpline(Cubic(Natural(OnCell())))
         ni = NoInterp()
-        
-        # ax = axionModes(coords,modes,freqs,tilts)
-        # itpax = interpolate(ax,(ni,bc,bc,ni))
-        # sitpax = scale(itpax,1:length(freqs),tilts,tilts,1:ML)
 
         # Spline for distance interpolation
         for i in eachindex(freqs), j in eachindex(distances)
@@ -216,6 +188,13 @@ mutable struct GrandPropagationMatrix
         itpP_d = interpolate(p_d,(ni,bc,ni,ni))
         sitpP_d = scale(itpP_d,1:length(freqs),distances,1:ML,1:ML)
 
+        # Spline for disk propagation
+        for i in eachindex(freqs), j in eachindex(distances)
+            p_disk[i,j,:,:] .= propagationCoeffs(freqs[i],distances[j],0.0,0.0,eps,modes,coords)
+        end
+        itpP_disk = interpolate(p_disk,(ni,bc,ni,ni))
+        sitpP_disk = scale(itpP_disk,1:length(freqs),distances,1:ML,1:ML)
+
         # Spline for tilt interpolation
         for i in eachindex(freqs), j in eachindex(tilts), k in eachindex(tilts)
             p_t[i,j,k,:,:] .= propagationCoeffs(freqs[i],0.0,tilts[j],tilts[k],1.0,modes,coords)
@@ -223,53 +202,59 @@ mutable struct GrandPropagationMatrix
         itpP_t = interpolate(p_t,(ni,bc,bc,ni,ni))
         sitpP_t = scale(itpP_t,1:length(freqs),tilts,tilts,1:ML,1:ML)
 
-        new(freqs,M,L,ML,sitpP_d,sitpP_t)
+        new(freqs,thickness,eps,tand,nm,M,L,ML,sitpP_d,sitpP_t,sitpP_disk)
     end
 end
 
 
 const GPM = GrandPropagationMatrix
 
-function construct_from_spline(gpm::GrandPropagationMatrix, 
+function construct_from_spline(
+        gpm::GrandPropagationMatrix,
+        P::ScaledInterpolation, 
         f::Real, 
         distance::Real;)
     """
     Interpolates the propagation matrix for a given frequency and distance.
     """
-    P = Array{ComplexF64}(undef,gpm.ML,gpm.ML)
+    mat = Array{ComplexF64}(undef,gpm.ML,gpm.ML)
 
     for i in 1:gpm.ML, j in 1:gpm.ML
-        P[i,j] = gpm.P_d(f, distance, i, j)
+        mat[i,j] = P(f, distance, i, j)
     end
 
-    return P
+    return mat
 end
 
-function construct_from_spline(gpm::GrandPropagationMatrix, 
+function construct_from_spline(
+        gpm::GrandPropagationMatrix,
+        P::ScaledInterpolation, 
         f::Real, 
         tiltx::Real, 
         tilty::Real;)
     """
     Interpolates the propagation matrix for a given frequency and tilts.
     """
-    P = Array{ComplexF64}(undef,gpm.ML,gpm.ML)
+    mat = Array{ComplexF64}(undef,gpm.ML,gpm.ML)
 
     for i in 1:gpm.ML, j in 1:gpm.ML
-        P[i,j] = gpm.P_t(f, tiltx, tilty, i, j)
+        mat[i,j] = P(f, tiltx, tilty, i, j)
     end
 
-    return P
+    return mat
 end
+
+ax = axionModes(coords,modes)
+
+gpm = GrandPropagationMatrix(freqs, modes, coords)
 
 # %%
 function transfer_matrix_3d(gpm::GrandPropagationMatrix,
         distances::Array{<:Real},
         tilts::Array{<:Real},
         ax::Vector{ComplexF64},
-        freqs::AbstractArray{<:Real},
-        modes::Modes,
-        coords::Coordinates;
-        eps=24.0,tand=0.,nm=1e30,thickness=1e-3, waveguide=true)
+        freqs::AbstractArray{<:Real};
+        waveguide=true)
     """
     Takes a GrandPropagationMatrix and returns the boost factor for a given set of distances, 
     relative tilts, axion modes, and frequencies. Eps, tand, nm, and thickness are optional 
@@ -282,8 +267,8 @@ function transfer_matrix_3d(gpm::GrandPropagationMatrix,
 
     B = zeros(ComplexF64,ML,length(freqs))
 
-    ϵ  = eps*(1.0-1.0im*tand); nd = sqrt(ϵ); nm = complex(nm); ϵm = nm^2
-    A  = 1-1/ϵ; A0 = 1-1/ϵm
+    eps  = gpm.eps*(1.0-1.0im*gpm.tand); nd = sqrt(eps); nm = complex(gpm.nm); ϵm = nm^2
+    A  = 1-1/eps; A0 = 1-1/ϵm
 
     G0 = G(ML,nm,1) # Reflection and transmission matrices for the mirror
     Gv = G(ML,1,nd) # Reflection and transmission matrices for the disk -> vacuum interface
@@ -295,53 +280,53 @@ function transfer_matrix_3d(gpm::GrandPropagationMatrix,
     T  = Matrix{ComplexF64}(I, 2*ML, 2*ML)
 
     Threads.@threads for j in 1:length(freqs)
-        MM = Matrix{ComplexF64}(I, 2*ML, 2*ML)*0
+        MM  = Matrix{ComplexF64}(I, 2*ML, 2*ML)*0
         tmp = Matrix{ComplexF64}(undef, 2*ML, 2*ML)
         Pvt = Matrix{ComplexF64}(I,2*ML,2*ML)
         Pvd = Matrix{ComplexF64}(I,2*ML,2*ML)
         Pv0 = Matrix{ComplexF64}(I,2*ML,2*ML)
-        Pd = Matrix{ComplexF64}(I,2*ML,2*ML)
+        Pd  = Matrix{ComplexF64}(I,2*ML,2*ML)
         
         # Propagation matrix for propagation inside disks.
-        Pd_ = cispi(+2*freqs[j]*nd*thickness/c0)
-        Pd = diagm([fill(conj(Pd_),ML); fill(Pd_,ML)])
+        Pd_ = cispi(+2*freqs[j]*nd*gpm.thickness/c0)
+        Pd  = diagm([fill(conj(Pd_),ML); fill(Pd_,ML)])
 
         # Tilt of the first disk.
-        Pv0_temp = construct_from_spline(gpm, j,-deg2rad(tilts[1,1]),-deg2rad(tilts[1,2]))
-        Pv0[1:ML,1:ML] .= Pv0_temp; Pv0[ML+1:2ML,ML+1:2ML] .= inv(Pv0_temp) 
+        Pv0[1:ML,1:ML] .= construct_from_spline(gpm, gpm.P_t, j,-deg2rad(tilts[1,1]),-deg2rad(tilts[1,2]))
+        Pv0[ML+1:2ML,ML+1:2ML] .= inv(Pv0[1:ML,1:ML]) 
 
-        T0 = copy(T) # * Pv0
+        T0 = copy(T) * Pv0
 
         # iterate in reverse order to sum up MM in single sweep (thx david)
         
         for i in Iterators.reverse(eachindex(distances))
             if waveguide == false
-                Pd_temp = propagationCoeffs(freqs[j],thickness,0.0,0.0,eps,modes,coords)
-                Pd[1:ML,1:ML] .= Pd_temp; Pd[ML+1:2ML,ML+1:2ML] .= inv(Pd_temp)
+                Pd[1:ML,1:ML] .= construct_from_spline(gpm, gpm.P_disk, j, gpm.thickness); Pd[ML+1:2ML,ML+1:2ML] .= inv(Pd[1:ML,1:ML])
             end
             # Distance propagation matrix
-            Pvd[1:ML,1:ML] .= construct_from_spline(gpm, j, distances[i]); Pvd[ML+1:2ML,ML+1:2ML] .= inv(Pvd[1:ML,1:ML])
+            Pvd[1:ML,1:ML] .= construct_from_spline(gpm, gpm.P_d, j, distances[i])
+            Pvd[ML+1:2ML,ML+1:2ML] .= inv(Pvd[1:ML,1:ML])
             # Tilt propagation matrix
-            Pvt[1:ML,1:ML] .= construct_from_spline(gpm, j, deg2rad(tilts[i,1]), deg2rad(tilts[i,2])); Pvt[ML+1:2ML,ML+1:2ML] .= inv(Pvt[1:ML,1:ML])
+            Pvt[1:ML,1:ML] .= construct_from_spline(gpm, gpm.P_t, j, deg2rad(tilts[i,1]), deg2rad(tilts[i,2]))
+            Pvt[ML+1:2ML,ML+1:2ML] .= inv(Pvt[1:ML,1:ML])
             
-            # MM += T0 * S                              # Construction of M
+            # MM += T0 * S
             axpy!(S, T0, MM)
-            # T0 *= Gd * Pd                             # T = Gd*Pd 
+            # T0 *= Gd * Pd
             mul!(tmp, T0, Gd)
             mul!(T0, tmp, Pd)
-            # MM -= T0 * S                              # Construction of M
-            axpy!(-S, T0, MM) 
+            # MM -= T0 * S
+            axpy!(-S, T0, MM)
 
-            # T0 *= Gv * Pvd * Pvt                      # T *= Gv * Pv
+            # T0 *= Gv * Pvd * Pvt                      
             mul!(tmp, T0, Gv)
             mul!(T0, tmp, Pvd)
             mul!(tmp, T0, Pvt)
-            mul!(T0, tmp, I)  # This line remains unchanged as it multiplies by the identity matrix
+            mul!(T0, tmp, I)
 
             if i == 1
                 # Reflection from the mirror at the end of the cavity
                 # MM += T0 * S0                         # Construction of M
-                T0 *= Pv0
                 axpy!(S0, T0, MM)
                 T0 *= G0                              # T *= G0
             end
@@ -364,13 +349,8 @@ function transfer_matrix_3d(gpm::GrandPropagationMatrix,
     return B
 end
 
-# %%
 
-ax = axionModes(coords,modes)
-
-gpm = GrandPropagationMatrix(freqs, modes, coords)
-#%%
-@time B = transfer_matrix_3d(gpm,dists,tilts,ax,freqs,modes,coords,waveguide=true)
+@time B = transfer_matrix_3d(gpm,dists,tilts,ax,freqs,waveguide=true)
 
 
 # abs2.(propagationCoeffs(freqs[500],7e-3,0,0,1.0,modes,coords))
@@ -389,92 +369,6 @@ display(graph1)
 # filepath = joinpath(filepath, filename)
 
 # savefig(filepath)
-
-# %%
-
-B = zeros(ComplexF64,M*(2L+1),length(freqs)); ML = M*(2L+1)
-# B = zeros(ComplexF64,length(freqs))
-M = 1; L = 1
-
-modes = Modes(coords,M,L);
-ax = axionModes(coords,modes)
-tiltx = deg2rad(0)
-tilty = deg2rad(0.1)
-
-eps = 24.; tand = 0; nm = 1e15
-ϵ  = eps*(1.0-1.0im*tand); nd = sqrt(ϵ); nm = complex(nm); ϵm = nm^2
-A  = 1-1/ϵ; A0 = 1-1/ϵm
-
-Id = Matrix{ComplexF64}(I, ML, ML)
-
-G0 = G(ML,nm,1)
-Gv = G(ML,1,nd)
-Gd = G(ML,nd,1)
-
-
-
-
-
-S  =  A/2*I(2ML)*diagm([ax; ax])
-S0 = A0/2*I(2ML)*diagm([ax; ax])
-
-@time for i in eachindex(freqs)
-    # T03 = G2*P2*G1*P1*G0*P0
-    # MM = T13*S0+T23*S1+T33*S2
-
- 
-    Pv_temp = propagationCoeffs(freqs[i],7e-3,tiltx,tilty,1.0,modes,coords)
-    Pv0_temp = propagationCoeffs(freqs[i],0,-tiltx,-tilty,1.0,modes,coords)
-    
-    Pv = Matrix{ComplexF64}(I,2*ML,2*ML)
-    Pv0 = Matrix{ComplexF64}(I,2*ML,2*ML)
-
-    Pv[1:ML,1:ML] .= Pv_temp; Pv[ML+1:2ML,ML+1:2ML] .= inv(Pv_temp)
-    Pv0[1:ML,1:ML] .= Pv0_temp; Pv0[ML+1:2ML,ML+1:2ML] .= inv(Pv0_temp)
-
-    Pd_ = cispi(+2*freqs[i]*nd*1e-3/c0)
-    Pd = diagm([fill(conj(Pd_),ML); fill(Pd_,ML)])
-    
-
-    T33 = Matrix{ComplexF64}(I, 2*ML, 2*ML) * Pv0
- 
-    T23 = T33 * Gd * Pd
- 
-    T13 = T23 * Gv * Pv
-
-    T03 = T13 * G0; T = T03
-
-    MM = T13 * S0 + (-T23 + T33) * S
-
-    M11 = MM[1:ML,1:ML]
-    M12 = MM[1:ML,ML+1:2ML]
-    M21 = MM[ML+1:2ML,1:ML]
-    M22 = MM[ML+1:2ML,ML+1:2ML]
-
-    T11 = T[1:ML,1:ML]
-    T12 = T[1:ML,ML+1:2ML]
-    T21 = T[ML+1:2ML,1:ML]
-    T22 = T[ML+1:2ML,ML+1:2ML]
-
-    B[:,i] = ((M11+M12) - T12*inv(T22)*(M21+M22))*ones(ML)
-
-end; 
-
-plot(freqs/1e9,abs2.(B)'; label=["L2=-1" "L2= 0" "L2= 1"])
-
-# %%
-
-# filename = "Hardcode_1Disk_no_tilt.svg"
-# filepath = joinpath(@__DIR__, "Plots_for_Meeting/")
-# filepath = joinpath(filepath, filename)
-# savefig(filepath)
-
-
-# note: mistakenly swapping T33 for T13 in 
-# MM = (T13.*S0).*ax - (T23.*S).*ax + (T33.*S).*ax
-# produces current TM3d result
-
-
 # %%
 
 using BoostFractor
